9fdc8f062ffa0ccf09f79f4d577de043eb4142bd
baojiong20176 <baojiong20176@hundsun.com>
2020-09-01 21:15:57 +0800

    add jvm.classloader.MyTest6

---
+++ src/main/java/com/vic/demo/jvm/classloader/MyTest5.java
@@ -0,0 +1,14 @@
+package com.vic.demo.jvm.classloader;
+
+/**
+ * 源程序名称：MyTest5 <br>
+ * 源程序包名：com.vic.demo.jvm.classloader <br>
+ * 系统名称：demo <br>
+ * 开发时间：2020/8/31 10:07 下午 <br>
+ *
+ * @Author baojiong20176 <br>
+ * <br>
+ * Copyright © 2020 Hundsun Technologies Inc. All Rights Reserved
+ */
+public class MyTest5 {
+}
