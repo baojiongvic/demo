e599f0e3a8ccd51788577fb667fc645e84b98db1
baojiong20176 <baojiong20176@hundsun.com>
2020-09-03 23:32:43 +0800

    MyTest5 add example

--- src/main/java/com/vic/demo/jvm/classloader/MyTest5.java
+++ src/main/java/com/vic/demo/jvm/classloader/MyTest5.java
@@ -8,20 +8,50 @@ package com.vic.demo.jvm.classloader;
  *
  * @Author baojiong20176 <br>
  * <br>
- *
+ * <p>
  * 当一个接口在初始化时，并不要求其父接口都完成初始化，
  * 只有在真正使用到父接口的时候（如引用接口中所定义的常量时）才会初始化
  */
 public class MyTest5 {
     public static void main(String[] args) {
-        System.out.println(MyChild5.i);
+//        System.out.println(MyChild5.b);
+        System.out.println(MyParent5A.thread);
     }
 }
 
-interface MyParent5 {
-    int i = 1;
+interface MyGrandParent5 {
+    public static Thread thread = new Thread() {
+        {
+            System.out.println("MyGrandParent5 invoked");
+        }
+    };
+
+}
+
+interface MyParent5 extends MyGrandParent5 {
+    public static Thread thread = new Thread() {
+        {
+            System.out.println("My Parent5 invoked");
+        }
+    };
 }
 
 class MyChild5 implements MyParent5 {
-    int i2 = 2;
+    static int b = 2;
+}
+
+interface MyGrandPa5A {
+    public static final Thread thread = new Thread() {
+        {
+            System.out.println("MyGrandPa5A invoked");
+        }
+    };
+}
+
+interface MyParent5A extends MyGrandPa5A {
+    public static final Thread thread = new Thread() {
+        {
+            System.out.println("MyParent5A invoked");
+        }
+    };
 }
