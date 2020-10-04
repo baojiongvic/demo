5a61ee92d2fbb058f7996084a7724d2a3e74f5e9
baojiongvic <95152394@qq.com>
2020-10-04 10:52:42 +0800

    modify MyTest16
    自定义类加载器复杂场景测试类

---
+++ src/main/java/com/vic/practice/jvm/classloader/MySample.java
@@ -0,0 +1,12 @@
+package com.vic.practice.jvm.classloader;/**
+ * 源程序名称：MySample <br> 
+ * 源程序包名：com.vic.practice.jvm.classloader <br>
+ * 系统名称：practice <br>
+ * 软件著作权：恒生电子股份有限公司 版权所有 <br>
+ * 开发时间：2020/10/4 2:00 下午 <br>
+ * 功能描述：
+ * @author baojiong20176 <br>
+ * <br>
+ */
+public class MySample {
+}
