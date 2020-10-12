ba1c1d8e4327a3ef4fafd92752418a2faecada96
baojiongvic <95152394@qq.com>
2020-10-12 22:36:45 +0800

    扩展类加载器介绍例子

--- src/main/java/com/vic/practice/jvm/classloader/MyTest22.java
+++ src/main/java/com/vic/practice/jvm/classloader/MyTest22.java
@@ -1,12 +1,24 @@
-package com.vic.practice.jvm.classloader;/**
- * 源程序名称：MyTest22 <br> 
+package com.vic.practice.jvm.classloader;
+
+/**
+ * 源程序名称：MyTest22 <br>
  * 源程序包名：com.vic.practice.jvm.classloader <br>
  * 系统名称：practice <br>
- * 软件著作权：恒生电子股份有限公司 版权所有 <br>
  * 开发时间：2020/10/12 10:15 下午 <br>
  * 功能描述：
+ *
  * @author baojiong20176 <br>
  * <br>
  */
 public class MyTest22 {
+
+    static {
+        System.out.println("MyTest22 initializer");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MyTest22.class.getClassLoader());
+
+        System.out.println(MyTest1.class.getClassLoader());
+    }
 }
