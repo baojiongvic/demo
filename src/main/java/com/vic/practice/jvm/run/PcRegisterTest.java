c8005bff13cfa28e72c98b16df236fcd9c2954ee
baojiongvic <95152394@qq.com>
2021-03-02 07:21:05 +0800

    add PcRegisterTest.java

---
+++ src/main/java/com/vic/practice/jvm/run/PcRegisterTest.java
@@ -0,0 +1,23 @@
+package com.vic.practice.jvm.run;
+
+/**
+ * 软件著作权：恒生电子股份有限公司 版权所有 <br>
+ * 系统名称：practice <br>
+ *
+ * @author baojiong20176
+ * @create 2021/3/2 7:12 上午
+ */
+public class PcRegisterTest {
+
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+
+        int k = i + j;
+
+        String s = "abc";
+
+        System.out.println(s);
+        System.out.println(k);
+    }
+}
