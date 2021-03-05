f67de698a65da33e1301be5aea1e01f6d4d4661c
baojiongvic <95152394@qq.com>
2021-03-05 08:23:15 +0800

    add OperandStackTest

---
+++ src/main/java/com/vic/practice/jvm/run/OperandStackTest.java
@@ -0,0 +1,14 @@
+package com.vic.practice.jvm.run;
+
+/**
+ * @author baojiong20176
+ * @create 2021/3/5 6:40 上午
+ */
+public class OperandStackTest {
+
+    public void testAddOperation() {
+        byte i = 15;
+        int j = 8;
+        int k = i + j;
+    }
+}
