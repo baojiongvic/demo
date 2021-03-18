145d6aaf62a959972070e85d24128b85d13311b3
baojiongvic <95152394@qq.com>
2021-03-18 10:22:13 +0800

    modify delete copyright

--- src/main/java/com/vic/practice/jvm/classloader/MyCat.java
+++ src/main/java/com/vic/practice/jvm/classloader/MyCat.java
@@ -4,7 +4,6 @@ package com.vic.practice.jvm.classloader;
  * 源程序名称：MyCat <br>
  * 源程序包名：com.vic.practice.jvm.classloader <br>
  * 系统名称：practice <br>
- * 软件著作权：恒生电子股份有限公司 版权所有 <br>
  * 开发时间：2020/10/4 1:57 下午 <br>
  * 功能描述：
  *
